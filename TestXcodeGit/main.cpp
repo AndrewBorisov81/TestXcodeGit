04c72d651602a3b39724131c5f7a135e73a92835
AndrewBorisov81 <ibobojohn81@gmail.com>
2020-12-11 11:17:36 +0200

    Initial Commit

---
+++ TestXcodeGit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TestXcodeGit
+//
+//  Created by Andrew Borisov on 11.12.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
